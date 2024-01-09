e9757044edc5d72026854b10fa17daf9bc559ee8
George W J Heath-Collins <33731624+Amesileo@users.noreply.github.com>
2024-01-09 16:31:40 +0000

    Initial commit
    Functional Home, History, Visit and Ringing Master pages.
    
    More JS to come, basic functionality enabled.

---
+++ main.js
@@ -0,0 +1,27 @@
+function goToHome() {
+    window.location = "./index.html";
+}
+
+function goToHistory() {
+    window.location = "./history.html";
+}
+
+function goToVisit() {
+    window.location = "./visit.html";
+}
+
+function goToRingingMasters() {
+    window.location = "./ringingMasters.html";
+}
+
+function goToTraining() {
+    window.location = "./training.html";
+}
+
+function goToContact() {
+    window.location = "./contact.html";
+}
+
+function goToLinks() {
+    window.location = "./links.html";
+}
