f9470fdc37ba2127881240f4cd11344471e20efe
George W J Heath-Collins <33731624+Amesileo@users.noreply.github.com>
2024-01-10 15:49:31 +0000

    Added new nav to all HTML, updated CSS, JS
    New Nav was added to all HTML pages, and CSS was adjusted to accommodate. Menu items were re-coloured to help users with navigation - The current page icon will be highlighted to stand out more than the blue hue used before.
    
    JavaScript function to move between pages was rewritten to use the element's ID as an input, and then pull that value into the link to navigate to the correct page.
    Additional JS function was used to toggle the dropdown menu, which is only used on mobile.

--- main.js
+++ main.js
@@ -1,40 +1,12 @@
-/*
-All of these just point to places.
-Needs rewriting to pull ElementId and use that to decide where to go.
-E F F I C I E N C Y 👀
-
-THIS WILL BE THE CORRECT FUNCTION:
-*/
 function goToPage(id) {
-    window.location = "./" + id.toString() + ".html";
+    window.location = "./" + id + ".html";
 }
 
-//Currently used functions are here!
-
-function goToHome() {
-    window.location = "./index.html";
-}
-
-function goToHistory() {
-    window.location = "./history.html";
-}
-
-function goToVisit() {
-    window.location = "./visit.html";
-}
-
-function goToRingingMasters() {
-    window.location = "./ringingMasters.html";
-}
-
-function goToTraining() {
-    window.location = "./training.html";
-}
-
-function goToContact() {
-    window.location = "./contact.html";
-}
-
-function goToLinks() {
-    window.location = "./links.html";
+function openDropdownMenu() {
+    let menu = document.getElementById("dropdownMenu");
+    if (menu.style.height > "0px"){
+        menu.style.height="0px"
+    } else {
+        menu.style.height="260px"
+    }
 }
