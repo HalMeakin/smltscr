19210b261831b5e7323e30dea1e39b8a175cbf93
George W J Heath-Collins <33731624+Amesileo@users.noreply.github.com>
2024-01-10 19:39:35 +0000

    Add files via upload

---
+++ hawkear/SMLT.20240107-1735.4/TouchViewer/rowfaults.js
@@ -0,0 +1,6 @@
+row stats based on model
+- peal speed, using the next wholepull's model, i.e. model predictions)
+- rms error
+- Lead error as % of IBG
+Faults on basis of gaps, so rms of the nBells-1 gaps, converted to a percentage of IBG
+  
